6940ed9e3ab9f963fb245f48f58fc0912b19a220
Sandra <sandradarriba007@gmail.com>
2025-04-29 10:15:45 +0200

    Arduino codigos

---
+++ primer codigo.ino
@@ -0,0 +1,18 @@
+Primer vídeo
+
+// El primer vídeo explica las partes de la placa
+
+Segundo vídeo
+
+void setup(){
+    pinMode (13, OUTPUT);     // pin 13 como salida.
+   }
+   
+   void loop() {
+     digitalWrite(13, HIGH);   // pin 13 a nivel alto (1)
+     delay(1000);              // demora 1 seg.
+     digitalWrite(13, LOW);    // pin 13 a nivel bajo (0)
+     delay(1000);              //demora de 1 seg.
+   
+   }
+   
---
+++ quinto codigo.ino
@@ -0,0 +1,26 @@
+Septimo vídeo:
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+int SENSOR = 2;
+int TEMPERATURA;
+int HUMEDAD;
+
+DHT dht(SENSOR, DHT22);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+}
+
+void loop() {
+  TEMPERATURA = dht.readTenperature();
+  HUMEDAD = DHT.reasHumidity();
+  Serial.print("Temperatura: ");
+  Serial.print(TEMPERATURA);
+  Serial.println("HUMEDAD: ");
+  Serial.println(HUMEDAD);
+  delay(500);
+}
